dae995e7cc08fb5a19ae1c2fd7c09ada20260e5f
Your Name <you@example.com>
2019-05-06 13:04:43 +0400

    classery zharangakanutyamb

---
+++ Das 2/script.js
@@ -0,0 +1,25 @@
+// let obj = {
+//     "first_name": "Adrine",
+//     "last_name": "Saghatelyan",
+//     "age": 15,
+//     "tumo_student": true,
+//     sayHello(){
+//         console.log("this.first_name");
+//     }
+   
+// }
+// console.log(obj);
+// console.log(obj.first_name);
+// obj.sayHello();
+
+// // class Student {
+// //     constructor() {
+// //         this.first_name = "Adrine";
+// //         this.last_name = "Saghatelyan";
+// //         this.age = 15;
+// //         this.tumo_student = true;
+// //     }
+// // }
+
+
+
