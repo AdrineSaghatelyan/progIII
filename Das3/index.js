ed51e819edeb6a8e85002ed6eb31e0cb0f92ec1b
Your Name <you@example.com>
2019-05-10 22:54:44 +0400

    Modulneri mej

--- Das3/index.js
+++ Das3/index.js
@@ -10,7 +10,41 @@ app.get("/:name", function(req, res){
     res.send("<h1>Hello " + name +"</h1>");
 });
 
-app.listen(3001, function(){
-    console.log("Example is running on port 3001");
+app.listen(3000, function(){
+    console.log("Example is running on port 3000");
  });
- 
+let Triangle = require("./module");
+let myTriangleObject = new Triangle(5);
+
+function main() {
+   console.log(myTriangleObject.getArea());
+}
+main();
+
+
+
+// let Square = require("./module");
+// let mySquareObject = new Square(5);
+
+// function main() {
+//    console.log(mySquareObject.getArea());
+// }
+// main();
+// module.exports = class Square {
+//     constructor(side) {
+//         this.side = side;
+//     }		
+//     getArea() {
+//         return this.side * this.side;
+//     }
+// };
+
+
+//var fs = require('fs');
+
+// function main(){
+//    var file  = "hello.txt";
+//    fs.appendFileSync(file, "Hello world\n");
+// }
+// main();
+
